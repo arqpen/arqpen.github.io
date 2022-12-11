58b5b1712cb2548b52a07e6f21dc909f75d61741
Saez Daniel <63934260+saezdaniel@users.noreply.github.com>
2022-12-11 14:56:15 -0500

    svg Colors Add

--- js/main.js
+++ js/main.js
@@ -6,26 +6,48 @@ $(window).scroll(function(){
   else sticky.removeClass('sticky');
 });
 
-//Color
-$('#colorpicker').on('input', function() {
-    $('#hexcolor').val(this.value);
+//Color 1
+$('#aqp-cpk-1').on('input', function() {
+    $('#hexc-c1').val(this.value);
     $("#aqpCl").css("fill", this.value);
 });
-$('#hexcolor').on('input', function() {
-  $('#colorpicker').val(this.value);
+$('#hexc-c1').on('input', function() {
+  $('#aqp-cpk-1').val(this.value);
   $("#aqpCl").css("fill", this.value);
 });
+//Color 2
+$('#aqp-cpk-2').on('input', function() {
+    $('#hexc-c2').val(this.value);
+    $("#aqpCd").css("fill", this.value);
+    $("#aqpLi, #aqpLo").css("stroke", this.value);
 
-$('#colorpicker2').on('input', function() {
-    $('#hexcolor2').val(this.value);
 });
-$('#hexcolor2').on('input', function() {
-  $('#colorpicker2').val(this.value);
+$('#hexc-c2').on('input', function() {
+  $('#aqp-cpk-2').val(this.value);
+  $("#aqpCd").css("fill", this.value);
+  $("#aqpLi, #aqpLo").css("stroke", this.value);
+
+});
+//Color 3
+$('#aqp-cpk-3').on('input', function() {
+    $('#hexc-c3').val(this.value);
+    $("#aqpCs").css("fill", this.value);
+});
+$('#hexc-c3').on('input', function() {
+  $('#aqp-cpk-3').val(this.value);
+  $("#aqpCs").css("fill", this.value);
 });
 
+//Opacity Shadow
+$('#shapeOpacity').on("change mousemove", function() {
+    var newval=$(this).val();
+    $("#shapeOpacityVar").text(newval+'%');
+     $("#aqpSh").css("opacity", newval/100);
+  });
 
 //open 
 
 $( ".gphs >div" ).click(function() {
   alert( "Handler for .click() called." );
 });
+
