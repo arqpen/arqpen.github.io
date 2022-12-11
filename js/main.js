56a17d53cc79820fe4fe82ed31803f6f4a0ce7d9
Saez Daniel <63934260+saezdaniel@users.noreply.github.com>
2022-12-11 12:45:31 -0500

    Detalles

--- js/main.js
+++ js/main.js
@@ -6,11 +6,19 @@ $(window).scroll(function(){
   else sticky.removeClass('sticky');
 });
 
+//Color
 $('#colorpicker').on('input', function() {
     $('#hexcolor').val(this.value);
 });
 $('#hexcolor').on('input', function() {
   $('#colorpicker').val(this.value);
+});
+
+$('#colorpicker2').on('input', function() {
+    $('#hexcolor2').val(this.value);
+});
+$('#hexcolor2').on('input', function() {
+  $('#colorpicker2').val(this.value);
 });
 
 
